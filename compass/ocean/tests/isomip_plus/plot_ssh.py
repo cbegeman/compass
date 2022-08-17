aa53734df4a9c2894f9203bc7e73ea7cada665e4
Carolyn Begeman <cbegeman@lanl.gov>
2022-08-17 16:23:42 -0500

    Update temp viz

--- compass/ocean/tests/isomip_plus/plot_ssh.py
+++ compass/ocean/tests/isomip_plus/plot_ssh.py
@@ -3,21 +3,25 @@ import numpy
 import matplotlib.pyplot as plt
 
 #path = '/lcrc/group/e3sm/ac.cbegeman/scratch/MPAS-Ocean-output/testcases_bugfixes-ramp_thin-film_chrys_2/ocean/isomip_plus/2km/single_layer/thin_film_Ocean0'
-path = '/lcrc/group/e3sm/ac.cbegeman/scratch/MPAS-Ocean-output/testcases_master_thin-film_chrys/ocean/isomip_plus/2km/single_layer/thin_film_Ocean0'
-#ds = xarray.open_dataset(f'{path}/simulation/output.nc')
-ds = xarray.open_dataset(f'{path}/performance/output.nc')
-dsMesh = xarray.open_dataset(f'{path}/simulation/init.nc')
-dsForcing = xarray.open_dataset(f'{path}/simulation/forcing_data_init.nc')
+path = '/lcrc/group/e3sm/ac.cbegeman/scratch/MPAS-Ocean-output/testcases_master_thin-film_chrys/ocean/isomip_plus/5km/single_layer/Ocean0'
+step = 'simulation'
+ds = xarray.open_dataset(f'{path}/{step}/output.nc')
+dsMesh = xarray.open_dataset(f'{path}/{step}/init.nc')
+dsForcing = xarray.open_dataset(f'{path}/{step}/forcing_data_init.nc')
 
 ssh = ds.ssh
+landIcePressureInit = dsMesh.landIcePressure
 layerThickness = ds.layerThickness
 #landIcePressure = ds.landIcePressure
 xCell = dsMesh.xCell
 yCell = dsMesh.yCell
-nt = ds.sizes['Time']
+nt = min(ds.sizes['Time'],10)
 
 tidalInputMask = dsForcing.tidalInputMask
 mask = tidalInputMask.values==1
+print(f'sum(mask)={numpy.sum(mask)}')
+openOceanMask = landIcePressureInit.values[0,:]==0
+print(f'sum(openOceanMask)={numpy.sum(openOceanMask)}')
 
 time = ds.daysSinceStartOfSim.values
 #days = time.astype('timedelta64[D]').astype(float)
@@ -31,29 +35,40 @@ print(f'yMin,yMean,yMax: {numpy.min(y.values)},{numpy.mean(y.values)},{numpy.max
 fig = plt.figure()
 for tidx in range(nt):
     sshForcing = ssh[tidx,mask]
-    #print(f'sshMin,sshMean,sshMax: {numpy.min(sshForcing.values)},{numpy.mean(sshForcing.values)},{numpy.max(sshForcing.values)}')
-    #print(f'plot: {time[tidx]},{numpy.mean(sshForcing.values)}')
-    plt.plot(days[tidx],numpy.mean(sshForcing.values))
+    sshOpenOcean = ssh[tidx,openOceanMask]
+    print(f'sshMin,sshMean,sshMax: {numpy.min(sshForcing.values)},{numpy.mean(sshForcing.values)},{numpy.max(sshForcing.values)}')
+    print(f'plot: {days[tidx]},{numpy.nanmean(sshForcing.values)}')
+    plt.plot(days[tidx],numpy.nanmean(sshForcing.values),'-',label='boundary')
+    plt.plot(days[tidx],numpy.nanmean(sshOpenOcean.values),'--',label='open ocean')
+plt.legend()
 plt.savefig(f'{path}/viz/plots/ssh_t.png')
 plt.close(fig)
 
 # Plot time evolution of layer thickness in thin film region
 thinFilmMask = xCell<450e3
-fig = plt.figure()
-for tidx in range(nt):
-    h = layerThickness[tidx,thinFilmMask,:]
-    #print(f'sshMin,sshMean,sshMax: {numpy.min(sshForcing.values)},{numpy.mean(sshForcing.values)},{numpy.max(sshForcing.values)}')
-    #print(f'plot: {days[tidx]},{numpy.mean(h.values)}')
-    plt.plot(days[tidx],numpy.mean(h.values),'.k')
-plt.savefig(f'{path}/viz/plots/h_thin_film_t.png')
-plt.close(fig)
+print(f'type(layerThickness)={type(layerThickness)}')
+print(f'shape(layerThickness)={numpy.shape(layerThickness)}')
+print(f'shape(thinFilmMask)={numpy.shape(thinFilmMask)}')
 
-fig = plt.figure()
-for tidx in range(nt):
-    h = ssh[tidx,thinFilmMask]
-    plt.plot(days[tidx],numpy.mean(h.values),'.k')
-plt.savefig(f'{path}/viz/plots/ssh_thin_film_t.png')
-plt.close(fig)
+if numpy.sum(thinFilmMask.values) > 0:
+    fig = plt.figure()
+    for tidx in range(nt):
+        h = layerThickness[tidx,thinFilmMask,:].values
+        print(f'shape(h)={numpy.shape(h)}')
+        #h = h.flatten()
+        #print(f'shape(h.flatten)={numpy.shape(h)}')
+        #print(f'sshMin,sshMean,sshMax: {numpy.min(sshForcing.values)},{numpy.mean(sshForcing.values)},{numpy.max(sshForcing.values)}')
+        #print(f'plot: {days[tidx]},{numpy.mean(h.values)}')
+        plt.plot(days[tidx],numpy.mean(h),'.k')
+    plt.savefig(f'{path}/viz/plots/h_thin_film_t.png')
+    plt.close(fig)
+
+    fig = plt.figure()
+    for tidx in range(nt):
+        h = ssh[tidx,thinFilmMask]
+        plt.plot(days[tidx],numpy.mean(h.values),'.k')
+    plt.savefig(f'{path}/viz/plots/ssh_thin_film_t.png')
+    plt.close(fig)
 
 #fig = plt.figure()
 #for tidx in range(nt):
