59f7f3827f48f5b73f3ec5c6cc4da0714dc0d889
Carolyn Begeman <cbegeman@lanl.gov>
2022-10-27 14:28:14 -0500

    Add time-varying thin-film cases

--- compass/ocean/tests/isomip_plus/__init__.py
+++ compass/ocean/tests/isomip_plus/__init__.py
@@ -40,3 +40,10 @@ class IsomipPlus(TestGroup):
                                   experiment=experiment,
                                   vertical_coordinate=vertical_coordinate,
                                   thin_film_present=True))
+                for vertical_coordinate in ['sigma', 'single_layer']:
+                    self.add_test_case(
+                        OceanTest(test_group=self, resolution=resolution,
+                                  experiment=experiment,
+                                  vertical_coordinate=vertical_coordinate,
+                                  time_varying_forcing=True,
+                                  thin_film_present=True))
